b0d3881d342bc7959dc978646c7277e3e254a2af
mistlog <54229343+mistlog@users.noreply.github.com>
2020-01-22 04:51:02 +0000

    Deploying to gh-pages from  - a0213b6e878a1fd15864a65775b951dbd0fdc078 🚀

---
+++ precache-manifest.cc2bfc375345906f8ba713b8f17b8842.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119cf117295f9478861a519ec03934cf",
+    "url": "/typedraft-playground/index.html"
+  },
+  {
+    "revision": "db2037f04476c3dcfd04",
+    "url": "/typedraft-playground/static/js/2.42e886d3.chunk.js"
+  },
+  {
+    "revision": "8eb23e0e9ba20b089aad",
+    "url": "/typedraft-playground/static/js/main.9d451821.chunk.js"
+  },
+  {
+    "revision": "12f7053f52697f9659b5",
+    "url": "/typedraft-playground/static/js/runtime~main.757fef17.js"
+  }
+]);
